e860314a15053aff98297812218264ae66e6a545
Marcel Santing <root>
2017-11-14 20:28:50 +0100

    testing xml reader

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,22 +15,25 @@ $(window).on('load', function(){
         xmlhttp.open("GET", "progress.xml", false);
         xmlhttp.send();
         xmlDoc = xmlhttp.responseXML;
-        var temp_table1 = "";
-        var temp_table2 = "";
+        var task_table1 = "";
+        var task_table2 = "";
+        var issue_table1 = "";
+        var issue_table2 = "";
 
-        temp_table1 = "<thead><TR style='text-align:center;font-weight: bold;'><th>FEATURE</th><th>DESCRIPTION</th><th>STATUS</th></TR></thead>";
+        task_table1 = "<thead><TR style='text-align:center;font-weight: bold;'><th>FEATURE</th><th>DESCRIPTION</th><th>STATUS</th></TR></thead>";
 
         var x = xmlDoc.getElementsByTagName("TASK");
         for (i = 0; i < x.length; i++) {
-            temp_table2 = temp_table2 + '<tr><td>' + (x[i].getElementsByTagName("FEATURE")[0].childNodes[0].nodeValue) + '</td><td>' + x[i].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + '</td><td>' + x[i].getElementsByTagName("STATUS")[0].childNodes[0].nodeValue + '</td></tr>';
+            task_table2 = task_table2 + '<tr><td>' + (x[i].getElementsByTagName("FEATURE")[0].childNodes[0].nodeValue) + '</td><td>' + x[i].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + '</td><td>' + x[i].getElementsByTagName("STATUS")[0].childNodes[0].nodeValue + '</td></tr>';
         }
-        $("#xml_tasks").append("<table>" + temp_table1 + "<tbody>"+ temp_table2 + "</tbody></table>");
+        $("#xml_tasks").append("<table>" + task_table1 + "<tbody>"+ task_table2 + "</tbody></table>");
 
-        var x = xmlDoc.getElementsByTagName("ISSUE");
-        for (i = 0; i < x.length; i++) {
-            temp_table2 = temp_table2 + '<tr><td>' + (x[i].getElementsByTagName("FEATURE")[0].childNodes[0].nodeValue) + '</td><td>' + x[i].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + '</td><td>' + x[i].getElementsByTagName("STATUS")[0].childNodes[0].nodeValue + '</td></tr>';
+        issue_table1 = "<thead><TR style='text-align:center;font-weight: bold;'><th>FEATURE</th><th>DESCRIPTION</th><th>STATUS</th></TR></thead>";
+        var y = xmlDoc.getElementsByTagName("ISSUE");
+        for (i = 0; i < y.length; i++) {
+            issue_table2 = issue_table2 + '<tr><td>' + (y[i].getElementsByTagName("FEATURE")[0].childNodes[0].nodeValue) + '</td><td>' + y[i].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + '</td><td>' + y[i].getElementsByTagName("STATUS")[0].childNodes[0].nodeValue + '</td></tr>';
         }
-        $("#xml_issues").append("<table>" + temp_table1 + "<tbody>"+ temp_table2 + "</tbody></table>");
+        $("#xml_issues").append("<table>" + issue_table1 + "<tbody>"+ issue_table2 + "</tbody></table>");
 
     }
 
